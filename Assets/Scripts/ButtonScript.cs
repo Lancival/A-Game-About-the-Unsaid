fc377e067ff9ae90c3449c0cf96eb20a62459fc7
Richard Cheng <rhocheng@g.ucla.edu>
2020-01-29 18:42:15 -0800

    First commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+	[SerializeField] private Button button;
+	[SerializeField] private string sceneName;
+
+    void Start() {
+    	button.onClick.AddListener(ChangeScene);
+    }
+
+    void ChangeScene() {
+    	SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/MessageScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MessageScript : MonoBehaviour
+{
+    [SerializeField] private Button button;
+
+    void Start() {
+    	button.onClick.AddListener(AddMessage);
+    }
+
+    void AddMessage() {
+    	return;
+    }
+}
