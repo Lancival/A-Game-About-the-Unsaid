7f3d3d7606a421bc153799e70727f52610a5ef26
Richard Cheng <rhocheng@g.ucla.edu>
2020-01-30 19:13:18 -0800

    Added basic message-sending capability

--- Assets/Scripts/MessageScript.cs
+++ Assets/Scripts/MessageScript.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 
 public class MessageScript : MonoBehaviour
 {
-    [SerializeField] private Button button;
+    [SerializeField] private Button button; // Button which will add a message when clicked
+    [SerializeField] private GameObject chatMessagePrefab; // Chat message prefab to be instantiated
+    [SerializeField] private GameObject chatContainer; // Object which contains all of the chat messages
+    private string message = "Example message! Replace me with actual dialogue!";
 
     void Start() {
+    	// Allows the button to activate the AddMessage function when clicked
     	button.onClick.AddListener(AddMessage);
     }
 
-    void AddMessage() {
-    	return;
+    public void AddMessage() {
+    	var newMessage = Instantiate(chatMessagePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    	newMessage.transform.SetParent(chatContainer.transform, false);
+    	newMessage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(message);
     }
 }
