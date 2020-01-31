4814d45c84b5d3f399304cf5b689aa4efaaa1491
Richard Cheng <rhocheng@g.ucla.edu>
2020-01-30 19:37:24 -0800

    Added clarifying comments

--- Assets/Scripts/MessageScript.cs
+++ Assets/Scripts/MessageScript.cs
@@ -9,18 +9,23 @@ public class MessageScript : MonoBehaviour
     [SerializeField] private Button button; // Button which will add a message when clicked
     [SerializeField] private GameObject chatMessagePrefab; // Chat message prefab to be instantiated
     [SerializeField] private GameObject chatContainer; // Object which contains all of the chat messages
-    private string message = "Example message! Replace me with actual dialogue!";
+    private string message = "Example message! Replace me with actual dialogue!"; // Chat message to use
 
     void Start() {
     	// Allows the button to activate the AddMessage function when clicked
     	button.onClick.AddListener(AddMessage);
     }
 
+    // Scroll the chat to the bottom to view the new chat message
+    public void ScrollToBottom() {
+        GameObject.Find("Scroll View").GetComponent<ScrollRect>().verticalNormalizedPosition = -6.5E-2F; // Magic number obtained through testing
+    }
+
+    // Add a new chat message
     public void AddMessage() {
-    	var newMessage = Instantiate(chatMessagePrefab, new Vector3(0, 0, 0), Quaternion.identity);
-    	newMessage.transform.SetParent(chatContainer.transform, false);
-    	newMessage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(message);
-        newMessage.transform.parent.parent.parent.gameObject.GetComponent<ScrollRect>().verticalNormalizedPosition = 0;
-        newMessage.transform.parent.parent.parent.gameObject.GetComponent<ScrollRect>().verticalNormalizedPosition = -6.5E-2F;
+    	var newMessage = Instantiate(chatMessagePrefab, new Vector3(0, 0, 0), Quaternion.identity); // Create new chat message
+    	newMessage.transform.SetParent(chatContainer.transform, false); // Place message inside chat container
+    	newMessage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(message); // Change text to dialogue
+        ScrollToBottom();
     }
 }
