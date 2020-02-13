b2d957cfb9554fb3ef4dcca78e302259c95e1f6b
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-12 18:59:29 -0800

    Initial chat dialogue option system created

--- Assets/Scripts/MessageScript.cs
+++ Assets/Scripts/MessageScript.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class MessageScript : MonoBehaviour
-{
+public class MessageScript : MonoBehaviour {
+
     [SerializeField] private Button button; // Button which will add a message when clicked
     [SerializeField] private GameObject chatMessagePrefab; // Chat message prefab to be instantiated
-    [SerializeField] private GameObject chatContainer; // Object which contains all of the chat messages
-    private string message = "Example message! Replace me with actual dialogue!"; // Chat message to use
+
+    private float SCROLLCONSTANT = -6.5E-2F; // Magic number obtained through testing
 
     void Start() {
-    	// Allows the button to activate the AddMessage function when clicked
-    	button.onClick.AddListener(AddMessage);
+    	button.onClick.AddListener(AddMessage); // Allows the button to activate the AddMessage function when clicked
     }
 
     // Scroll the chat to the bottom to view the new chat message
     public void ScrollToBottom() {
-        GameObject.Find("Scroll View").GetComponent<ScrollRect>().verticalNormalizedPosition = -6.5E-2F; // Magic number obtained through testing
+        GameObject.Find("Scroll View").GetComponent<ScrollRect>().verticalNormalizedPosition = SCROLLCONSTANT;
     }
 
     // Reset color of button
@@ -30,9 +29,9 @@ public class MessageScript : MonoBehaviour
     // Add a new chat message
     public void AddMessage() {
     	var newMessage = Instantiate(chatMessagePrefab, new Vector3(0, 0, 0), Quaternion.identity); // Create new chat message
-    	newMessage.transform.SetParent(chatContainer.transform, false); // Place message inside chat container
-    	newMessage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(message); // Change text to dialogue
+    	newMessage.transform.SetParent(GameObject.Find("Content").transform, false); // Place message inside chat container
+    	newMessage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text); // Change text to dialogue
         ScrollToBottom();
-        ResetButtonColor();
+        //ResetButtonColor();
     }
 }
