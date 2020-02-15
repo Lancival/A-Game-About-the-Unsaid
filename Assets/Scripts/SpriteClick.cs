4ab7048081bb194df1c3e91a5b31d012b402fd20
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-15 00:17:41 -0800

    Started work on Visual Novel dialogue, UI setup mostly finished

---
+++ Assets/Scripts/SpriteClick.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteClick : MonoBehaviour {
+    void OnMouseDown() {
+    	Debug.Log("Sprite Clicked! Replace this function to actually do something!");
+    }
+}
