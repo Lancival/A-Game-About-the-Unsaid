5b1d1e95ff1f68c9946d468b7ea77d5a8b1bc1b6
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-27 19:52:30 -0800

    Audio + bug fixes

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 static class CharacterInfo {
 	public static readonly string[] names =
 		{
-			"Player",	// ID = 0
-			"MainNPC",	// ID = 1
-			"ERROR"		// ID = 2
+			"June",		// ID = 0, main character
+			"Miles",	// ID = 1, character_description
+			"Rebecca",	// ID = 2, best friend
+			"ERROR"		// ID = 3, ERROR!!!!
 		};
 }
 
@@ -14,4 +15,5 @@ static class Settings {
 	public static float DIALOGUE_SPEED = 0.03f;
 	public static bool PAUSED = false;
 	public static float AUDIO_VOLUME = 1.0f;
+	public static int FONT_SCALE = 1; // 0 = small, 1 = normal, 2 = large
 }
